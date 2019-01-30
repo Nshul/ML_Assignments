c6d995ac16c257f3d05c3db0a2913ea441b3a961
Anshul Mittal <anshulmittal0409@gmail.com>
2019-01-30 17:14:04 +0530

    Handle interrupt
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- main3.py
+++ main3.py
@@ -8,51 +8,54 @@ m_ans = 0
 c_ans = 25
 
 # this event is called when user clicks on the graph
+
+
 def onclick(event):
     global clk_x, clk_y
     global m_ans, c_ans
-    clk_x, clk_y = event.xdata, event.ydata
-    coordx.append(clk_x)
-    coordy.append(clk_y)
-    y_calc = m_ans*clk_x+c_ans
-    cum_m = cum_c = 4
-    # for loop in range(100):
-    while abs(cum_m) > 0.000002 or abs(cum_c) > 0.000002:
-        # cum_error = 0
-        cum_m=0
-        cum_c=0
-        for itr in range(len(coordx)):
-            clk_x = coordx[itr]
-            clk_y = coordy[itr]
-            y_calc = m_ans*clk_x+c_ans
-            error = clk_y - y_calc
-            cum_m += 0.0001*error*clk_x
-            cum_c += 0.0001*error
-            # m_ans = m_ans + error*clk_x
-            # c_ans = c_ans + error
-            print("**")
-            print(m_ans)
-            print(c_ans)
-            print(error)
-            print("**")
-        m_ans+=(cum_m/len(coordx))
-        c_ans+=(cum_c/len(coordx))
-        
-        # clear previous plot
-        ax.cla()
+    try:
+        clk_x, clk_y = event.xdata, event.ydata
+        coordx.append(clk_x)
+        coordy.append(clk_y)
+        y_calc = m_ans*clk_x+c_ans
+        cum_m = cum_c = 4
+        # for loop in range(100):
+        while abs(cum_m) > 0.000002 or abs(cum_c) > 0.000002:
+            # cum_error = 0
+            cum_m = 0
+            cum_c = 0
+            for itr in range(len(coordx)):
+                clk_x = coordx[itr]
+                clk_y = coordy[itr]
+                y_calc = m_ans*clk_x+c_ans
+                error = clk_y - y_calc
+                cum_m += 0.0001*error*clk_x
+                cum_c += 0.0001*error
+                print("**")
+                print(m_ans)
+                print(c_ans)
+                print(error)
+                print("**")
+            m_ans += (cum_m/len(coordx))
+            c_ans += (cum_c/len(coordx))
 
-        # Scatter plot the clicked points
-        ax.scatter(coordx, coordy)
+            # clear previous plot
+            ax.cla()
 
-        # draw this line
-        tempx = np.arange(0, 50)
-        tempy = m_ans*tempx+c_ans
-        ax.plot(tempx, tempy)
+            # Scatter plot the clicked points
+            ax.scatter(coordx, coordy)
 
-        plt.axis([0, 50, 0, 50])
-        plt.draw()
-        plt.pause(0.001)
+            # draw this line
+            tempx = np.arange(0, 50)
+            tempy = m_ans*tempx+c_ans
+            ax.plot(tempx, tempy)
 
+            plt.axis([0, 50, 0, 50])
+            plt.draw()
+            plt.pause(0.001)
+    except KeyboardInterrupt:
+        print("Interrupt")
+        sys.exit()
     # plt.show()
 
 
