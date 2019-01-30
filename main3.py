f7bd96be83b0926bc4b34ee98ba4a252b693d5b8
Anshul Mittal <anshulmittal0409@gmail.com>
2019-01-30 17:08:47 +0530

    Add assign3 for gradient descent
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- main3.py
+++ main3.py
@@ -1,19 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 coordx = []
 coordy = []
 m_ans = 0
-c_ans = 0
-
-# # draw this line
-#         tempx = np.arange(0,100)
-#         tempy = m*tempx+c
-#         ax.plot(tempx,tempy)
+c_ans = 25
 
 # this event is called when user clicks on the graph
-
-
 def onclick(event):
     global clk_x, clk_y
     global m_ans, c_ans
@@ -21,26 +15,45 @@ def onclick(event):
     coordx.append(clk_x)
     coordy.append(clk_y)
     y_calc = m_ans*clk_x+c_ans
-    error = clk_y - y_calc
-    m_ans = m_ans + 0.0001*error*clk_x
-    c_ans = c_ans + 0.0001*error
-    # m_ans = m_ans + error*clk_x
-    # c_ans = c_ans + error
-    print(m_ans)
-    print(c_ans)
-    # clear previous plot
-    ax.cla()
+    cum_m = cum_c = 4
+    # for loop in range(100):
+    while abs(cum_m) > 0.000002 or abs(cum_c) > 0.000002:
+        # cum_error = 0
+        cum_m=0
+        cum_c=0
+        for itr in range(len(coordx)):
+            clk_x = coordx[itr]
+            clk_y = coordy[itr]
+            y_calc = m_ans*clk_x+c_ans
+            error = clk_y - y_calc
+            cum_m += 0.0001*error*clk_x
+            cum_c += 0.0001*error
+            # m_ans = m_ans + error*clk_x
+            # c_ans = c_ans + error
+            print("**")
+            print(m_ans)
+            print(c_ans)
+            print(error)
+            print("**")
+        m_ans+=(cum_m/len(coordx))
+        c_ans+=(cum_c/len(coordx))
+        
+        # clear previous plot
+        ax.cla()
 
-    # Scatter plot the clicked points
-    ax.scatter(coordx, coordy)
+        # Scatter plot the clicked points
+        ax.scatter(coordx, coordy)
 
-    # draw this line
-    tempx = np.arange(0, 100)
-    tempy = m_ans*tempx+c_ans
-    ax.plot(tempx, tempy)
+        # draw this line
+        tempx = np.arange(0, 50)
+        tempy = m_ans*tempx+c_ans
+        ax.plot(tempx, tempy)
 
-    plt.axis([0, 100, 0, 100])
-    plt.show()
+        plt.axis([0, 50, 0, 50])
+        plt.draw()
+        plt.pause(0.001)
+
+    # plt.show()
 
 
 fig, ax = plt.subplots()
@@ -49,5 +62,6 @@ fig, ax = plt.subplots()
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
 # set axis for plot
-plt.axis([0,100,0,100])
-plt.show()
+plt.axis([0, 50, 0, 50])
+plt.ion()
+plt.show(block=True)
